6a75f167fdbafc86cadf0ec01f044d0894d41338
Ana Ventura <anaventura1811@gmail.com>
2024-11-04 10:23:26 -0300

    feat: add example to ocp - solid

--- 2_O_OCP/ocp_example.py
+++ 2_O_OCP/ocp_example.py
@@ -1,3 +1,21 @@
+'''
+    Classes criadas como exemplos de aplicação do
+    Princípio aberto/fechado ou
+    OCP (Open/Closed Principle)
+
+    "Um artefato de software deve ser aberto para extensão,
+    mas fechado para modificação" (p. 70)
+
+    "Uma boa arquitetura de software deve reduzir a quantidade
+    de código a ser mudado para o mínimo possível(...)
+    Primeiro, devemos separar adequadamente as coisas que mudam por
+    razões diferentes (o Princípio da Responsabilidade Única) para, então,
+    organizarmos as dependências entre essas coisas de forma apropriada
+    (o Princípio da Inversão de Dependência)"(p. 71)
+
+'''
+
+
 class Programmer:
     def make(self):
         print("Programmer is coding")
@@ -26,6 +44,8 @@ class Company:
 company = Company()
 seller = Seller()
 programmer = Programmer()
+rh = HumanResources()
 
 company.do_work(seller)
 company.do_work(programmer)
+company.do_work(rh)
